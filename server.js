acd1c58fb4dddb3262abf40010e752c9206b35fd
Zimniros <zimniros@protonmail.com>
2018-05-31 17:27:09 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const mongo_uri = process.env.MONGO_URI || "mongodb://localhost/Podcast";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongo_uri);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes declaration
+// import
+//use - routes(app)
+
+app.use((req, res) => {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
+app.listen(port);
+console.log("todo list RESTful API server started on: " + port);
